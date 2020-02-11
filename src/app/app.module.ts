0a0f06a7d989ebe345c2c3ec797f4121fa5234cd
Cam Dziurgot <camerondziurgot@gmail.com>
2020-02-10 20:05:14 -0500

    add simple list to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SimpleListComponent } from './components/simple-list/simple-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleListComponent
   ],
   imports: [
     BrowserModule
