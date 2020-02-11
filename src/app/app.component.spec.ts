cfe3634d6c4ce39b7f9859f1ed44fa6fe3e4d42c
Cam Dziurgot <camerondziurgot@gmail.com>
2020-02-10 20:03:26 -0500

    set tests to look for title alone

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-jasmine-spy-tests app is running!');
+    expect(compiled.querySelector('.content').textContent).toContain('ng-jasmine-spy-tests');
   });
 });
