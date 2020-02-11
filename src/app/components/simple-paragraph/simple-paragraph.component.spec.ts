7bae5f40e7517af7767068feb4b470739206e88f
Cam Dziurgot <camerondziurgot@gmail.com>
2020-02-11 00:58:16 -0500

    add mock for subscription

--- src/app/components/simple-paragraph/simple-paragraph.component.spec.ts
+++ src/app/components/simple-paragraph/simple-paragraph.component.spec.ts
@@ -1,32 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, TestBed } from '@angular/core/testing';
 import { SimpleParagraphComponent } from './simple-paragraph.component';
+import { SimpleDataService } from '../../services/simple-data.service';
 
 describe('SimpleParagraphComponent', () => {
   let component: SimpleParagraphComponent;
-  let fixture: ComponentFixture<SimpleParagraphComponent>;
+
+  const mockedPayload: string = 'mocked small response';
+  const mockedService = {
+    provide: SimpleDataService,
+    useValue: {
+      subcribeToSmallParagraphPayload: (self, successCallback) => {
+        successCallback(self, mockedPayload);
+      }
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SimpleParagraphComponent ]
+      declarations: [ SimpleParagraphComponent ],
+      providers: [ mockedService ]
     })
     .compileComponents();
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SimpleParagraphComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should have mocked data', () => {
-  //   const fixture = TestBed.createComponent(SimpleParagraphComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.paragraph-content').textContent).toContain('mocked data');
-  // });
+  it('should have mocked data', () => {
+    const fixture = TestBed.createComponent(SimpleParagraphComponent);
+    fixture.detectChanges();
+    const nativeElement = fixture.debugElement.nativeElement;
+    expect(nativeElement.querySelector('.paragraph-content').textContent).toContain(mockedPayload);
+  });
 });
